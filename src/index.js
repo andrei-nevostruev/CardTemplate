a3cb8a60827f641a11c859c53f23143cf6bc52b7
Andrei Nevostruev <andrei.nevostruev@gmail.com>
2022-07-09 21:00:47 +0300

    add the second way for createRoot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-); 
+// // root.render(
+// //   <StrictMode>
+// //     <App />
+// //   </StrictMode>,
+// // ); 
 
+createRoot(document.getElementById('root')).render(<App />);
